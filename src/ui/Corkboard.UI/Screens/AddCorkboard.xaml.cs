b0205454a3ed6bdcac7ecfdd464b65f21bf59445
BariumLab <bariuminc@gmail.com>
2018-11-11 10:40:07 -0800

    Implemented adding a corkboard.

--- src/ui/Corkboard.UI/Screens/AddCorkboard.xaml.cs
+++ src/ui/Corkboard.UI/Screens/AddCorkboard.xaml.cs
@@ -20,9 +20,70 @@ namespace Corkboard.UI.Screens
     /// </summary>
     public partial class AddCorkboard : Page
     {
-        public AddCorkboard()
+        public AddCorkboard(MainWindow window)
         {
             InitializeComponent();
+            MainWindow = window;
+            PopulateCategoryDropdown();
         }
+
+
+        private void VisibilityButton_Private_Checked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Visibility = Visibility.Visible;
+        }
+
+        private void VisibilityButton_Private_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Visibility = Visibility.Hidden;
+        }
+
+        #region focus events
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Text.Equals("Enter the password for the new Corkboard"))
+            {
+                PasswordBox.Text = string.Empty;
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Text.Equals(string.Empty))
+            {
+                PasswordBox.Text = "Enter the password for the new Corkboard";
+            }
+        }
+
+        private void TitleBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TitleBox.Text.Equals("Title"))
+            {
+                TitleBox.Text = string.Empty;
+            }
+        }
+
+        private void TitleBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TitleBox.Text.Equals(string.Empty))
+            {
+                TitleBox.Text = "Title";
+            }
+        }
+
+        #endregion
+
+        #region private
+
+        private MainWindow MainWindow { get; set; }
+
+        private void PopulateCategoryDropdown()
+        {
+            // call api to get categories
+            //CatecoryComboBox.Items = ;
+        }
+
+        #endregion
     }
 }
