c4d737fb9336d23d0b43ac025604547cf3bae98c
BariumLab <bariuminc@gmail.com>
2018-11-11 10:21:05 -0800

    Moved add corkboard to a page.

--- src/ui/Corkboard.UI/Screens/AddCorkboard.xaml.cs
+++ src/ui/Corkboard.UI/Screens/AddCorkboard.xaml.cs
@@ -10,14 +10,15 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Corkboard.UI.Popups
+namespace Corkboard.UI.Screens
 {
     /// <summary>
     /// Interaction logic for AddCorkboard.xaml
     /// </summary>
-    public partial class AddCorkboard : Window
+    public partial class AddCorkboard : Page
     {
         public AddCorkboard()
         {
