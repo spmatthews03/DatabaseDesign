a20857e693035902609cc052064c496fccfe1abd
BariumLab <bariuminc@gmail.com>
2018-11-11 14:16:39 -0800

    Updated list view.

--- src/Corkboard.UI/Screens/SearchPushpin.xaml.cs
+++ src/Corkboard.UI/Screens/SearchPushpin.xaml.cs
@@ -36,10 +36,30 @@ namespace Corkboard.UI.Screens
 
         private Home PreviousPage { get; set; }
 
+        private GridViewColumn CreateGridColumn(string value, double width, string newBinding = null)
+        {
+            newBinding = newBinding ?? value;
+            return new GridViewColumn
+            {
+                Header = value,
+                DisplayMemberBinding = new Binding(newBinding),
+                Width = width
+            };
+        }
+
         private void DisplayResults(string search)
         {
             // call api
             // display results in listview
+            var view = new GridView();
+            SearchResultsView.View = view;
+            view.Columns.Add(CreateGridColumn("Description", 243.667));
+            view.Columns.Add(CreateGridColumn("Corkboard", 243.667));
+            view.Columns.Add(CreateGridColumn("Owner", 243.667));
+
+            // call api
+            // add result from api
+            //SearchResultsView.ItemsSource = from api;
         }
 
         #endregion
