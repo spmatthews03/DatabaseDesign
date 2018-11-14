3bb585402f8fb1efca271b37ce0b4635390ec64c
BariumLab <bariuminc@gmail.com>
2018-11-13 19:57:37 -0800

    Made user a required parameter. This will be passed to the query.

--- src/Corkboard.API/Helpers/ModelHelpers/CorkboardHelper.cs
+++ src/Corkboard.API/Helpers/ModelHelpers/CorkboardHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Corkboard.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -6,9 +7,24 @@ using System.Linq;
 namespace Corkboard.API.Helpers
 {
     public static class CorkboardHelper
-
     {
-        public static List<Models.Corkboard> GetUsersCorkboards()
+        /// <summary>
+        /// Returns the list of recently updated corkboards. This includes strictly includes the corkboards
+        /// the current user follows or watches.
+        /// </summary>
+        /// <param name="currentUser">User to use for following/watching.</param>
+        /// <returns>Returns the list of corkboards, or an empty list.</returns>
+        public static List<Models.Corkboard> GetRecentUpdatedCorkboards(User currentUser)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Returns the list of corkboards the current user created.
+        /// </summary>
+        /// <param name="currentUser">User to retrieve corkboards for.</param>
+        /// <returns></returns>
+        public static List<Models.Corkboard> GetUsersCorkboards(User currentUser)
         {
             var corkboardRows = DatabaseHelper.ExecuteQuery("GET ALL CORKBOARDS FOR USER");
             var corkboardList = new List<Models.Corkboard>();
@@ -25,9 +41,9 @@ namespace Corkboard.API.Helpers
             return corkboardList;
         }
 
-        public static List<Models.Corkboard> GetUsersPublicCorkboards()
+        public static List<Models.Corkboard> GetUsersPublicCorkboards(User user)
         {
-            return GetUsersCorkboards().Where(x => x.IsPrivate.Equals(false)).ToList();
+            return GetUsersCorkboards(user).Where(x => x.IsPrivate.Equals(false)).ToList();
         }
     }
 }
