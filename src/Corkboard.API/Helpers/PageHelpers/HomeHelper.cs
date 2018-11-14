602dd251ac7f289ca014998c409370bb2404e107
BariumLab <bariuminc@gmail.com>
2018-11-13 19:57:46 -0800

    Updated to use correct method.

--- src/Corkboard.API/Helpers/PageHelpers/HomeHelper.cs
+++ src/Corkboard.API/Helpers/PageHelpers/HomeHelper.cs
@@ -5,9 +5,13 @@ namespace Corkboard.API.Helpers.PageHelpers
 {
     public static class HomeHelper
     {
+        /// <summary>
+        /// Gets a list of recently updated corkboards.
+        /// </summary>
+        /// <returns>Returns the last four recently updated corkboards, or an empty list.</returns>
         public static List<Models.Corkboard> GetRecentlyUpdatedCorkboards()
         {
-            var corkboards = CorkboardHelper.GetUsersCorkboards();
+            var corkboards = CorkboardHelper.GetRecentUpdatedCorkboards();
             corkboards.OrderBy(x => x.LastUpdate);
             return corkboards.GetRange(0, 4);
 
