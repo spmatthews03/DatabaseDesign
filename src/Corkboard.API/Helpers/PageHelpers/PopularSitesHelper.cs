708cef1daabe4ca4e54560bf6bfe4477d17bb293
BariumLab <bariuminc@gmail.com>
2018-11-13 20:52:31 -0800

    Added api for getting popular sites.

---
+++ src/Corkboard.API/Helpers/PageHelpers/PopularSitesHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corkboard.API.Helpers.PageHelpers
+{
+    public static class PopularSitesHelper
+    {
+        /// <summary>
+        /// Returns the popular sites.
+        /// </summary>
+        public static List<Models.Sites> GetPopularSites()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Corkboard.API/Models/Sites.cs
@@ -0,0 +1,15 @@
+﻿namespace Corkboard.API.Models
+{
+    public class Sites
+    {
+        /// <summary>
+        /// Represents the pushpins that have the base site url.
+        /// </summary>
+        public int Pushpins { get; set; }
+
+        /// <summary>
+        /// Represents the base site url. i.e. www.amazon.com
+        /// </summary>
+        public string Site { get; set; }
+    }
+}
