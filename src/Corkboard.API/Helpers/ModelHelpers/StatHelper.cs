36c4dff4b03d1670ee6cdff6a864745b873370a3
BariumLab <bariuminc@gmail.com>
2018-11-13 20:44:56 -0800

    Added helper and model for stats.

---
+++ src/Corkboard.API/Helpers/ModelHelpers/StatHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Corkboard.API.Helpers.ModelHelpers
+{
+    public static class StatHelper
+    {
+        /// <summary>
+        /// Returns all stats for all users and their corkboards.
+        /// </summary>
+        public static Models.Stats GetCorkboardStats()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Corkboard.API/Models/Stats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corkboard.API.Models
+{
+    public class Stats
+    {
+        /// <summary>
+        /// Represents the amount of private corkboards for the user.
+        /// </summary>
+        public int PrivateCorkboards { get; set; }
+
+        /// <summary>
+        /// Represents the amount of private pushpins for the user.
+        /// </summary>
+        public int PrivatePushpins { get; set; }
+
+        /// <summary>
+        /// Represents the amount of public corkboards for the user.
+        /// </summary>
+        public int PublicCorkboards { get; set; }
+
+        /// <summary>
+        /// Represents the amount of public pushpins for the user.
+        /// </summary>
+        public int PublicPushpins { get; set; }
+
+        /// <summary>
+        /// Represents the user's full name.
+        /// </summary>
+        public string User { get; set; }
+    }
+}
