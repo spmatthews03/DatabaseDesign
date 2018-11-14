f3be8def3f7aa3f4f3a70ec356135bce7be9c2c7
BariumLab <bariuminc@gmail.com>
2018-11-13 19:58:32 -0800

    Added required user parameter.

--- src/Corkboard.API/Helpers/PageHelpers/HomeHelper.cs
+++ src/Corkboard.API/Helpers/PageHelpers/HomeHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Corkboard.API.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Corkboard.API.Helpers.PageHelpers
@@ -8,10 +9,11 @@ namespace Corkboard.API.Helpers.PageHelpers
         /// <summary>
         /// Gets a list of recently updated corkboards.
         /// </summary>
+        /// <param name="currentUser">Represents the currently logged in user.</param>
         /// <returns>Returns the last four recently updated corkboards, or an empty list.</returns>
-        public static List<Models.Corkboard> GetRecentlyUpdatedCorkboards()
+        public static List<Models.Corkboard> GetRecentlyUpdatedCorkboards(User currentUser)
         {
-            var corkboards = CorkboardHelper.GetRecentUpdatedCorkboards();
+            var corkboards = CorkboardHelper.GetRecentUpdatedCorkboards(currentUser);
             corkboards.OrderBy(x => x.LastUpdate);
             return corkboards.GetRange(0, 4);
 
