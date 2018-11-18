59318ed87b29dc27b2eec18c97c3a8dc07bf8930
BariumLab <bariuminc@gmail.com>
2018-11-17 20:13:30 -0800

    Added functionality for navigating to the pushpin.

--- src/Corkboard.UI/Screens/SearchPushpin.xaml.cs
+++ src/Corkboard.UI/Screens/SearchPushpin.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Corkboard.API.Helpers.PageHelpers;
+﻿using Corkboard.API.Helpers;
+using Corkboard.API.Helpers.PageHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace Corkboard.UI.Screens
     /// <summary>
     /// Interaction logic for SearchPushpin.xaml
     /// </summary>
-    public partial class SearchPushpin : Page
+    public partial class SearchPushpin : Page, IPage
     {
         public SearchPushpin(Home previousPage, string searchText)
         {
@@ -28,14 +29,42 @@ namespace Corkboard.UI.Screens
             DisplayResults(searchText);
         }
 
+        public MainWindow MainWindow => PreviousPage.MainWindow;
+        public Page Self => this;
+
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             PreviousPage.MainWindow.Navigate(PreviousPage);
         }
 
+        private void SearchResultsView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count < 1)
+            {
+                return;
+            }
+
+            var view = sender as ListView;
+            var properties = ConvertSelectedItem(view.SelectedItem);
+            var pushpin = PushpinHelper.GetPushpin(properties["Title"], properties["OwnerEmail"], properties["Url"], properties["DateTime"]);
+            PreviousPage.MainWindow.Navigate(new ViewPushpin(this, pushpin, PreviousPage.User));
+        }
+
         #region private
 
         private Home PreviousPage { get; set; }
+
+        private Dictionary<string, string> ConvertSelectedItem(object item)
+        {
+            var properties = item.GetType().GetProperties();
+            var dictionary = new Dictionary<string, string>();
+            foreach (var prop in properties)
+            {
+                dictionary.Add(prop.Name, prop.GetValue(item, null).ToString());
+            }
+
+            return dictionary;
+        }
 
         private GridViewColumn CreateGridColumn(string value, double width, string newBinding = null)
         {
