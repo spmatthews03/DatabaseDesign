f8192e65ec5d01d4da275c5ed54be8eb11f0cc48
WINDOWS-DK64USA\spmatthews03 <spmatthews03@gmail.com>
2018-11-18 18:20:34 -0800

    completed populat sites

--- src/Corkboard.API/Helpers/PageHelpers/PopularSitesHelper.cs
+++ src/Corkboard.API/Helpers/PageHelpers/PopularSitesHelper.cs
@@ -15,12 +15,12 @@ namespace Corkboard.API.Helpers.PageHelpers
         public static List<Models.Sites> GetPopularSites()
         {
             // TODO - we should not limit this
-            var results = DatabaseHelper.ExecuteQuery("SELECT url AS Site, COUNT(*) AS PushPins FROM pushpin WHERE tags IS NOT NULL GROUP BY url ORDER BY COUNT(*) DESC; ");
+            var results = DatabaseHelper.ExecuteQuery("SELECT site, COUNT(*) AS Count FROM sitename Group BY site ORDER BY COUNT(*) DESC ");
             var sites = new List<Models.Sites>();
             foreach (DataRow row in results.Rows)
             {
                 var url = row.GetValueInRow("Site");
-                var pushpins = Convert.ToInt32(row.GetValueInRow("PushPins"));
+                var pushpins = Convert.ToInt32(row.GetValueInRow("count"));
 
                 sites.Add(new Models.Sites(pushpins, url));
             }
